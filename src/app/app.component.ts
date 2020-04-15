88b40383ae46dd04fa46d71129cf0b2fbcb5ecea
cafajardop <carlosafp84@hotmail.com>
2020-04-14 23:00:01 -0500

    Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre: string = 'Capitán América';
+  cursos: string[] = ['JavaScript', 'NodeJS', 'React', 'Angular', 'Mongo', 'CSharp'];
+  PI: number = Math.PI;
+  porcentaje: number = 0.234;
+  salario: number = 1234.5;
+  fecha: Date = new Date();
+
+  valorPromesa = new Promise<string>((resolve) => {
+
+    setTimeout(() => {
+      resolve('LLego la data');
+    }, 4500);
+
+  });
+
+  heroe = {
+    nombre: 'Carlos',
+    clave: 'Desarrollador Full Stack',
+    edad: 23,
+    direccion: {
+      calle: 'Primera',
+      casa: 20
+    }
+  }
 }
